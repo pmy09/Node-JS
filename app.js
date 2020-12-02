6bb29029498d8e4304b61478c57854fac55faeab
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-02 14:29:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 // const routes = require('./routes');
 
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -53,7 +55,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // server.listen(3000);
