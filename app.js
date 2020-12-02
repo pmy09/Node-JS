2c28b3c09efbf70433ce23c8f24cfb2027121ec2
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-02 16:13:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // console.log(routes.someText);
 
