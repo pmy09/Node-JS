22cc8fd85a7fb3402d5c663d752504789452ef49
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-02 11:33:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 // const routes = require('./routes');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 // console.log(routes.someText);
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs!');
+//     next();
+// });
 
 // app.use((req, res, next) => {
 //     console.log('In the middleware!');
@@ -28,15 +31,21 @@ app.use('/', (req, res, next) => {
 // const server = http.createServer(app);
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another mega middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
-  });
+    // console.log('In another mega middleware!');
+    // res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
   
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
+    // console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
 // server.listen(3000);
 
-app.listen(3000)
+app.listen(8000)
