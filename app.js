835b383dcaab7477f245418a10fc90473f4461b4
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-02 12:38:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 // console.log(routes.someText);
@@ -30,20 +33,27 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // const server = http.createServer(app);
 
-app.use('/add-product', (req, res, next) => {
-    // console.log('In another mega middleware!');
-    // res.send('<h1>The "Add Product" Page</h1>');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+// app.use('/add-product', (req, res, next) => {
+//     // console.log('In another mega middleware!');
+//     // res.send('<h1>The "Add Product" Page</h1>');
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// });
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+// app.post('/product', (req, res, next) => {
+//     console.log(req.body);
+//     res.redirect('/');
+// });
   
-app.use('/', (req, res, next) => {
-    // console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+// app.use('/', (req, res, next) => {
+//     // console.log('In another middleware!');
+//     res.send('<h1>Hello from Express!</h1>');
+// });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 // server.listen(3000);
