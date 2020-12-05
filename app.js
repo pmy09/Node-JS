861457e9af18ca755424d4bdb909837e03598cd0
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-05 15:21:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
