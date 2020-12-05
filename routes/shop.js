10007bc635e9fce3e7566eb9a2477e32348e7d83
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-12-05 15:20:59 -0700

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,14 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
+// router.get('/', (req, res, next) => {
+//   console.log('shop.js', adminData.products);
+//   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+// });
+
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
